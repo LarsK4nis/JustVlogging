3361e91fbfbcbbb3a28b19cf09e42c85a0167ee9
0t4peo <83510245+LarsK4nis@users.noreply.github.com>
2023-11-10 17:25:47 +0100

    m2

--- app/models.py
+++ app/models.py
@@ -11,7 +11,24 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    # Relación con los posts (un usuario tiene muchos posts)
+  # Relación con los posts (un usuario tiene muchos posts)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-   
+    # Métodos para el manejo de contraseñas, autenticación, etc. pueden ser añadidos aquí
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Follower(db.Model):
+    __tablename__ = 'followers'
+
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True)
+
+  
