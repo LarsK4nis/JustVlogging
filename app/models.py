ab4b7a3e5b2699750ca2cf171d12f9ded5f1bc69
0t4peo <83510245+LarsK4nis@users.noreply.github.com>
2023-11-10 17:25:29 +0100

    m1

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relación con los posts (un usuario tiene muchos posts)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+   
