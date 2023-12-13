7a7b098ff21aabb495ccb819da88fe06c7fbdad9
0t4peo <83510245+LarsK4nis@users.noreply.github.com>
2023-12-13 19:43:14 +0100

    Forms and route edit_profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from .models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
@@ -18,7 +20,21 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Post')
 
+
+
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Este nombre de usuario ya está en uso. Por favor, elige otro.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Este correo electrónico ya está en uso. Por favor, elige otro.')
