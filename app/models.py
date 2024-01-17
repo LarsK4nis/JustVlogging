c75e5433873a5185bc949cf928757eba1f980c2e
0t4peo <83510245+LarsK4nis@users.noreply.github.com>
2024-01-17 17:50:12 +0100

    Creating Admin Panel. DDBB modelling

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
